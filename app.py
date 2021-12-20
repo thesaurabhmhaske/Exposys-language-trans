436c6aec0736d88c21e4ae5feaae7a5890d338d7
Saurabh Mhaske <85241921+saurabhmhaske011@users.noreply.github.com>
2021-12-20 12:58:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from googletrans import Translator
+trans = Translator()
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/translate')
+def translate(data, srclang, destlang):
+    mytext = data
+  
+    
+    t = trans.translate(mytext,
+    src = srclang ,
+    dest = destlang)
+    print(f'{t.origin}->{t.text}')
+    outputText = f'{t.text}'
+
+    return render_template('home.html', prediction=outputText)
+
+if __name__=='__main__':
+    app.run(debug=True)
