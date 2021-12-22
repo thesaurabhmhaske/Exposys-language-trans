e176e54727fac9175d06a52d88fd59d772998d94
Saurabh Mhaske <85241921+saurabhmhaske011@users.noreply.github.com>
2021-12-22 16:16:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request,redirect
 from googletrans import Translator
-trans = Translator()
-
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
-    return render_template('home.html')
+    return render_template("index.html")
 
-@app.route('/translate')
-def translate(data, srclang, destlang):
-    mytext = data
-  
-    
-    t = trans.translate(mytext,
-    src = srclang ,
-    dest = destlang)
-    print(f'{t.origin}->{t.text}')
-    outputText = f'{t.text}'  
-    return render_template('home.html', outputText)    
+@app.route("/trans",methods=["GET", "POST"])
+def trans():
+    if request.method == "POST":
+     t_sentence = request.form["sentence"]
+     language = request.form['inputvalue']
+     output = Translator().translate(t_sentence, dest=language)
+     text = output.text
+    else:
+        return render_template("home.html")
+    return render_template('home.html',output=text,sentence=t_sentence)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
