d8c67a43b33de034d147dddaa231a30a7194ea1a
Saurabh Mhaske <85241921+saurabhmhaske011@users.noreply.github.com>
2021-12-23 13:00:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,flash
 from googletrans import Translator
 app = Flask(__name__)
 
@@ -17,6 +17,19 @@ def trans():
         return render_template("home.html")
     return render_template('home.html',output=text,sentence=t_sentence)
 
+@app.route("/contact",methods=["POST"])
+def contact():
+    name = request.form['name']
+    email = request.form['email']
+    number = request.form['number']
+    subject = request.form['subject']
+    message = request.form['message']
+    return render_template('home.html', name=name)
+
+@app.route("/subscribe",methods=["POST"])
+def subscribe():
+    email = request.form['email']
+    return render_template('home.html',email=email)
 
 if __name__ == '__main__':
     app.run(debug=True)
